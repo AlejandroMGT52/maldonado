9e6f0434634133bcd54baf6911ba03b1c007f38e
AlejandroMGT52 <alejo.m15097@gmail.com>
2025-10-22 16:05:11 -0500

    Subido con el test

--- tests/test_suma.py
+++ tests/test_suma.py
@@ -1,4 +1,4 @@
 from app import sumar
 
 def test_suma_correcta():
-    assert sumar(2, 2) == 6 # ❌ intencionalmente incorrecto para fallo inicial
+    assert sumar(2, 2) == 4 # ❌ intencionalmente incorrecto para fallo inicial
